4b30a401e79c0b692a32c33db4fd016f62993f7e
minegrass <yapxh2004@hotmail.com>
2022-11-18 15:34:56 +0000

    intial with typescript & tailwind

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import "./app"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Pull up!</Text>
+      <Text className={'text-red-500'}>Paull up!</Text>
       <StatusBar style="auto" />
     </View>
   );
